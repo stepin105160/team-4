9e814f33ef3568b981a376b30f73c4e500ddf50a
stepin104955 <69070462+stepin104955@users.noreply.github.com>
2021-02-04 15:36:04 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,8 +3,8 @@ int main(){
 printf("This is Midhun M Menon \n");
 printf("This is R Sanjana\n");
 printf("This is Anurag\n");
-printf("This is Vinay\n");
 printf("This is Alen Reji Kokkad \n");  
 printf("This is Sai Keerthi \n");
 return 0;
 }
+printf("This is Vinay\n");
