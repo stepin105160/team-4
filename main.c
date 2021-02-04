a53afd13b382a5d7728eea5f3c89bea9cb3afb9f
anusha <anushavc0219@gmail.com>
2021-02-04 15:31:22 +0530

    new

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ printf("This is Midhun M Menon \n");
 printf("This is R Sanjana\n");
 printf("This is Anurag\n");
 printf("This is Vinay\n");
-printf("This is Alen Reji Kokkad\n");  
+printf("This is Alen Reji Kokkad\n"); 
+printf("This is Anusha\n"); 
 return 0;
 }
