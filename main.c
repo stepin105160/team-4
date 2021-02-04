ac4e737eefe8cd5dedee513015a4c003f84ab704
Alen Reji Kokkad <69103817+stepin104484@users.noreply.github.com>
2021-02-04 15:28:45 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,6 @@ printf("This is Midhun M Menon \n");
 printf("This is R Sanjana\n");
 printf("This is Anurag\n");
 printf("This is Vinay\n");
-printf("This is Alen Reji Kokkad");  
+printf("This is Alen Reji Kokkad\n");  
 return 0;
 }
